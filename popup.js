611d099336c043b8bc21e0e5cf52d0fcd8baedb4
Omar El-Dalil <omar.eldaleel@gmail.com>
2018-03-31 23:43:53 +0200

    Make the extension works from the popup window

--- popup.js
+++ popup.js
@@ -2,16 +2,37 @@ document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('#clearBtn').addEventListener('click',function () {
         chrome.tabs.query({active : true , currentWindow : true}, function (tabs) {
             chrome.tabs.executeScript(tabs[0].id , {
-                code : `document.getElementById("scrollbtn").style.display = "none";
-                        document.getElementById("resetbtn").style.display = "none";`
+                code : `document.getElementById('pane-side').scrollTop = 0;`
             })
         })
     })
     document.querySelector('#showBtn').addEventListener('click',function () {
         chrome.tabs.query({active : true , currentWindow : true}, function (tabs) {
             chrome.tabs.executeScript(tabs[0].id , {
-                code : `document.getElementById("scrollbtn").style.display = "block";
-                        document.getElementById("resetbtn").style.display = "block";`
+                code :
+                   `var scrollInterval = null,res=[];
+                    function contScroll() {
+                        let ot= {off: document.getElementById('pane-side').scrollTop}
+                        document.querySelectorAll('._25Ooe span._1wjpf').forEach(function(nameElement){
+                            if(res.indexOf(nameElement.innerText) == -1){
+                                res.push(nameElement.innerText)
+                            }
+                        }) ;
+                        document.getElementById('pane-side').scrollTop +=500;
+                        if(document.getElementById('pane-side').scrollTop == ot.off){
+                            res.sort();
+                            let resEnd = res.join('\\r\\n');
+                            let inp = document.createElement("textarea");
+                            inp.value = resEnd;
+                            document.body.appendChild(inp);
+                            inp.select();
+                            document.execCommand("Copy");
+                            clearInterval(scrollInterval);
+                            alert('Every thing is Copied');
+                        }
+                    }
+            
+                    scrollInterval = setInterval(contScroll,50);`
             })
         })
     })
